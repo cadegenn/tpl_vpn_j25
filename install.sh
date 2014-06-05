ccf880c9d1d5f12053293a98b182f0d0df46e8ff
DEGENNES Charles-Antoine <cadegenn@gmail.com>
2014-06-05 21:18:51 +0200

    ajout de la gestion des langues

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 DIRNAME=$(cd `dirname $0`; pwd)
 cp -a "${DIRNAME}"/* "/var/www/html/www.apasdeloup-joomla.org/templates/vpn/"
+cp -a "${DIRNAME}"/language/* "/var/www/html/www.apasdeloup-joomla.org/language/"
